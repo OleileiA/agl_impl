e79f3ef569f5b8f12b515258070021cdae007987
cyw <18604469853@163.com>
2020-10-20 22:58:48 +0800

    feat: 判断树是否对齐

---
+++ src/com/company/LC/初级算法/Tree/LevelOrder.java
@@ -0,0 +1,28 @@
+package com.company.LC.初级算法.Tree;
+
+import java.util.List;
+
+/*
+*
+* 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+*     3
+   / \
+  9  20
+    /  \
+   15   7
+
+   [
+  [3],
+  [9,20],
+  [15,7]
+]
+* */
+public class LevelOrder {
+
+    /*
+    *
+    * */
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        return null;
+    }
+}
