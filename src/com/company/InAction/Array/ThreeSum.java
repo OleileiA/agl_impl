586f6aecdebb02c076fc1df3a23f5076650e8d60
cyw <18604469853@163.com>
2020-11-05 00:14:17 +0800

    Three sum

--- src/com/company/InAction/Array/ThreeSum.java
+++ src/com/company/InAction/Array/ThreeSum.java
@@ -1,5 +1,7 @@
 package com.company.InAction.Array;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -17,8 +19,42 @@ https://leetcode-cn.com/problems/3sum/
 
 * */
 public class ThreeSum {
-    public List<List<Integer>> threeSum(int[] nums) {
+    /*
+    * 两数之和：a + b = target
+    * 三数之和 a + b = -c
+    *
+    * 三数之和就是两数之和的进阶版本:
+    * 1. 可以利用hash表和针对target循环的方式
+    * 2. 排序之后的双指针
+    * */
+    public static List<List<Integer>> threeSum(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> result = new ArrayList<>();
+        if (len < 3) return result;
+        Arrays.sort(nums);
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = len - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0) right--;
+                else if (sum < 0) left++;
+                else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return result;
+    }
 
-        return null;
+    public static void main(String[] args) {
+        ThreeSum threeSum = new ThreeSum();
+        threeSum.threeSum(new int[]{-1, 0, 1, 2, -1, -4});
     }
 }
