69e73db7a5c12ea1a04f68820b0f88219c71132e
cyw <18604469853@163.com>
2020-12-06 19:01:02 +0800

    min stack

---
+++ src/com/company/InAction/stack/MinStack.java
@@ -0,0 +1,54 @@
+package com.company.InAction.stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/*
+https://leetcode-cn.com/problems/min-stack/
+设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+
+push(x) —— 将元素 x 推入栈中。
+pop() —— 删除栈顶的元素。
+top() —— 获取栈顶元素。
+getMin() —— 检索栈中的最小元素。
+
+* */
+public class MinStack {
+
+    private Deque<Integer> stack;
+
+    private Deque<Integer> minStack;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new LinkedList<>();
+        minStack = new LinkedList<>();
+        minStack.push(Integer.MAX_VALUE);
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        minStack.push(Math.min(x, minStack.peek()));
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
